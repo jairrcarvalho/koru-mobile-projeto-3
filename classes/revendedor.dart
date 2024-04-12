77b238717eb68855311bf9645e38ce738125b4ef
Everton <evertonaugust42@gmail.com>
2024-04-12 12:59:02 -0300

    fix: :bug: ajuste de metodo
    foi realizado um ajuste na classe revendedor, vai utilizar o try/cach ao chamar o método realizarVenda.

--- classes/revendedor.dart
+++ classes/revendedor.dart
@@ -37,7 +37,11 @@ class Revendedor extends Pessoa {
   }
 
   void venderProduto(Produto produto) {
-    produto.realizarVenda();
-    produtosVendidos.add(produto);
+    try {
+      produto.realizarVenda();
+      produtosVendidos.add(produto);
+    } catch (e) {
+      throw e; 
+    }
   }
 }
