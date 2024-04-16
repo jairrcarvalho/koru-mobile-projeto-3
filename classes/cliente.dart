7c269dabccdbb80ae59035ae795c274502443f56
Wellington <dev.wellingtonandrade@gmail.com>
2024-04-16 16:58:24 -0300

    feat: :sparkles: Feita a função "verResumo" e "verProdutosComprados" na calsse cliente e revendedor
    Foi feita funções de resumos, "verResumo" e "verProdutosComprados" na calsse cliente e revendedor na qual mostra todas as informações resumidas.

--- classes/cliente.dart
+++ classes/cliente.dart
@@ -31,27 +31,27 @@ class Cliente extends Pessoa {
   }
 
   void comprarProduto(Produto produto, Revendedor revendedor) {
-    try{
+    try {
       late double? dinheiroPreCompra = dinheiro;
       if (dinheiro >= produto.valor) {
         revendedor.venderProduto(produto);
         dinheiro = dinheiroPreCompra - produto.valor;
         String valorArredondado = produto.valor.toStringAsFixed(2);
         String dinheiroPreCompraArredondado =
-        dinheiroPreCompra.toStringAsFixed(2);
+            dinheiroPreCompra.toStringAsFixed(2);
         String dinheiroArredondado = dinheiro.toStringAsFixed(2);
         produtosComprados.add(produto);
         print("Valor do produto: R\$ " + valorArredondado);
         print("Saldo do cliente: R\$ " + dinheiroPreCompraArredondado);
         print("Saldo do cliente após a compra: R\$ " + dinheiroArredondado);
         for (var produto in produtosComprados) {
-        print("Produto comprado: ${produto.nome}");
+          print("Produto comprado: ${produto.nome}");
         }
       } else {
-      throw Exception(
-          "$nome não possui dinheiro suficiente para comprar o produto ${produto.nome}.");
-     }
-    }catch(e){
+        throw Exception(
+            "$nome não possui dinheiro suficiente para comprar o produto ${produto.nome}.");
+      }
+    } catch (e) {
       print("Erro ao efetuar a comprar: $e");
     }
   }
@@ -77,7 +77,15 @@ class Cliente extends Pessoa {
     return total;
   }
 
-  void ordenarProdutosComprados(List<Produto> produtosComprados){
+  void ordenarProdutosComprados(List<Produto> produtosComprados) {
     produtosComprados.sort((a, b) => a.nome.compareTo(b.nome));
+  }
+
+  void verProdutosComprados() {
+    List<Produto> produtosOrdenados = produtosComprados;
+    print('Produtos comprados por $nome:');
+    for (var produto in produtosOrdenados) {
+      print('- Produto: ${produto.nome}, Valor:${produto.valor} reais');
+    }
   }
 }
--- classes/revendedor.dart
+++ classes/revendedor.dart
@@ -14,11 +14,10 @@ class Revendedor extends Pessoa {
     required this.matricula,
     required Genero genero,
   }) : super(
-          nome: nome,
-          dataDeNascimento: dataDeNascimento,
-          cpf: cpf,
-          genero: genero,
-        );
+            nome: nome,
+            dataDeNascimento: dataDeNascimento,
+            cpf: cpf,
+            genero: genero);
 
   @override
   void falar(String mensagem) {
@@ -60,12 +59,22 @@ class Revendedor extends Pessoa {
     return totalLucro;
   }
 
-  double calcularMediaProdutosVendidos() {
-    if (produtosVendidos.isEmpty) {
-      return 0.0;
-    }
-
+  void verResumo() {
     double totalVendido = calcularTotalVendido();
-    return totalVendido / produtosVendidos.length;
+    double lucro = calcularLucro();
+
+    double media = totalVendido / produtosVendidos.length;
+
+    try {
+      if (media.isNaN) {
+        throw FormatException('Não tem como calcular a média sendo 0.');
+      }
+    } catch (e) {
+      media = 0.0;
+      print('Erro ao calcular a média: $e');
+    }
+    print(
+      'O total vendido por $nome foi ${totalVendido.toStringAsFixed(2)} reais e a média aritmética de valor dos produtos vendidos é ${media.toStringAsFixed(2)} reais. O lucro recebido foi de ${lucro.toStringAsFixed(2)} reais.',
+    );
   }
 }
